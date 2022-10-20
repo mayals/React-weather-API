6c722efe8ea8f245d00d94e04296074933d74850
mayals <developer.web.dj@gmail.com>
2022-10-20 21:38:44 +0300

    some html design

--- src/components/ContentComp.js
+++ src/components/ContentComp.js
@@ -1,21 +1,24 @@
 // import {Container} from 'react-bootstrap';
 
+import { Container } from "react-bootstrap";
+
 const ContentComp = (props) =>{
   console.log(props.WeatherState)
 
                         return (
+                        <Container>
                             <div>
-                            { props.WeatherState?.temp } <br/>
-                            { props.WeatherState?.city } <br/>
-                            { props.WeatherState?.desc } <br/>
-                            { props.WeatherState?.humidity } <br/>
-                            { props.WeatherState?.pressure } <br/>
-                            { props.WeatherState?.wind } <br/>
-                            { props.WeatherState?.visibility } <br/>
-                            { props.WeatherState?.error } <br/>
-                            { props.WeatherState?.toast_message } <br/>
+                                <p>{ props.WeatherState?.temp }</p> 
+                                <p>{ props.WeatherState?.city }</p> 
+                                <p>{ props.WeatherState?.desc } </p>
+                                <p>{ props.WeatherState?.humidity }</p>  
+                                <p>{ props.WeatherState?.pressure }</p> 
+                                <p>{ props.WeatherState?.wind } </p>
+                                <p>{ props.WeatherState?.visibility } </p>
+                                <p>{ props.WeatherState?.error }</p>
+                                <p>{ props.WeatherState?.toast_message }</p> 
                             </div>
-                       
+                       </Container> 
                         );
 }
 export default ContentComp
